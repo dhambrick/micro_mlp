44cc82ea6e047ed7083d9d59258eec1fe06d1f7f
Dominick <Dominick>
2020-01-28 00:43:36 -0600

    Added Forward Pass Logic

--- micro_mlp.py
+++ micro_mlp.py
@@ -17,13 +17,28 @@ def LoadANNConfigFile(config_path):
 
 def InitializeMLP(cfg):
     for layer in cfg['Layers']:
-        print(layer['name'])
         m = layer['InputDim']
         n = layer['OutputDim']
         layer['weights'] = np.random.rand(n,m)
     return cfg
 
+def ComputeLayerResponse(layer_input,layer):
+    print('Layer name', layer['name'])
+    print('Input: ',layer_input.shape)
+    print('Weights', layer['weights'].shape)
+    layer_output = np.matmul(layer['weights'],layer_input)
+    layer_output = fct_map[layer["ActivationFunction"]](layer_output)
+    return layer_output
+
+def ComputeForwardPass(net_input,net_cfg):
+    x0 = net_input
+    for layer in net_cfg['Layers']:
+        if layer["name"] == 'Input':
+            x = x0
+        else:
+            x = ComputeLayerResponse(x,layer)
+    return x        
+
 cfg = LoadANNConfigFile("sine_mlp.yml")
 cfg = InitializeMLP(cfg)
-for layer in cfg['Layers']:
-        print(layer['weights'])
+print(ComputeForwardPass(np.asarray([0]),cfg))
