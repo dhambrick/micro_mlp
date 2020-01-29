c9903662148a4b5d27e1fd8150c14378042ed9b0
dhambrick <dom_ham@PTI-4365.peopletec.com>
2020-01-29 14:44:30 -0600

    Added Jupyter Notebook

--- micro_mlp.py
+++ micro_mlp.py
@@ -9,7 +9,8 @@ def relu(X):
 
 def mse(A,B):
     return (np.square(A - B)).mean()
-
+def xavier(m,n):
+    return (2*np.random.rand(m,n)-1)*np.sqrt(6./(m+n))
 
 fct_map = {"sigmoid":sigmoid , "relu":relu}
 
@@ -23,13 +24,13 @@ def InitializeMLP(cfg):
     for layer in cfg['Layers']:
         m = layer['InputDim']
         n = layer['OutputDim']
-        layer['weights'] = np.random.rand(n,m)
+        layer['weights'] = xavier(m,n)
     return cfg
 
 def ComputeLayerResponse(layer_input,layer):
-    print('Layer name', layer['name'])
-    print('Input: ',layer_input.shape)
-    print('Weights', layer['weights'].shape)
+    #print('Layer name', layer['name'])
+    #print('Input: ',layer_input.shape)
+    #print('Weights', layer['weights'].shape)
     layer_output = np.matmul(layer['weights'],layer_input)
     layer_output = fct_map[layer["ActivationFunction"]](layer_output)
     return layer_output
@@ -50,14 +51,14 @@ def WeightPerturbationTrain(train_data_x,
    for x,y in  zip(train_data_x,train_data_labels):
        A = ComputeForwardPass(x,net_cfg)
        E = mse(A,y)
-       print('x: ',x)
-       print('_y: ',A)
-       print('y: ',y)
-       print('MSE: ',E)
+       #print('x: ',x)
+       #print('_y: ',A)
+       #print('y: ',y)
+       #print('MSE: ',E)
        for layer in net_cfg['Layers']:
             if layer["name"] == 'Input':
                 continue
-            print('Layer: ',layer['name'])
+            #print('Layer: ',layer['name'])
             m,n = layer['weights'].shape
             layer['delta_weights'] = np.zeros((m,n))
             for i in range(m):
@@ -72,14 +73,9 @@ def WeightPerturbationTrain(train_data_x,
                     layer['weights'][i][j] = layer['weights'][i][j] - training_par['etta']
                     layer['delta_weights'][i][j] = beta*delta_E
             layer['weights'] = layer['weights'] + layer['delta_weights']
-            print('Perturbed Weights:')
-            print(layer['delta_weights'])
+            #print('Perturbed Weights:')
+            #print(layer['delta_weights'])
        
        
 
 
-cfg = LoadANNConfigFile("sine_mlp.yml")
-cfg = InitializeMLP(cfg)
-t = np.linspace(0,np.pi/2)
-y = np.sin(t)
-WeightPerturbationTrain(t,y,cfg)
